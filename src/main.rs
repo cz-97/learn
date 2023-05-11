d5901f8ed8cc2313b2f0393787286f2510299f62
cz-97 <caozheng97@outlook.com>
2023-05-11 15:32:57 +0800

    ""

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::fs::{read_to_string, write};
 use std::io;
 
+/// > 进入SumatraPDF后打开高级设置, 生成SumatraPDF-settings.txt
+/// 
+/// > 然后将change-theme.exe放在可执行文件所在目录
 fn main() -> io::Result<()> {
+
     let path = "./SumatraPDF-settings.txt";
 
     let colors = [
@@ -16,19 +20,20 @@ fn main() -> io::Result<()> {
             "BackgroundColor = #3B3B3B",
         ],
     ];
+
     let s = read_to_string(path)?;
 
-    let x: usize = match s.find(colors[0][1]) {
+    let x = match s.find(colors[0][1]) {
         Some(_) => 0,
         None => 1,
     };
 
-    let s = s
-        .replace(colors[x][0], colors[1 - x][0])
-        .replace(colors[x][1], colors[1 - x][1])
-        .replace(colors[x][2], colors[1 - x][2]);
-
-    write(path, s)?;
+    write(
+        path,
+        s.replace(colors[x][0], colors[1 - x][0])
+            .replace(colors[x][1], colors[1 - x][1])
+            .replace(colors[x][2], colors[1 - x][2]),
+    )?;
 
     Ok(())
 }
